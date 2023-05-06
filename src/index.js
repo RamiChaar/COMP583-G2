1066bc527d1041d9915cb4e0e0b0549ebda27ae7
RamiChaar <Chaarrami22@gmail.com>
2023-05-05 21:40:07 -0700

    styling change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Styling/Index.css';
+import './Styling/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
